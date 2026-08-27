b9011ae27b6a9c397b7134db23ec9f7a5bddcbfe
DarshanRevankar24 <darshanjbdvt24@gmail.com>
2026-08-27 13:55:52 +0530

    fix: correctly configure soft deletes for FastCRUD instances

--- backend/src/modules/family/crud.py
+++ backend/src/modules/family/crud.py
@@ -2,4 +2,4 @@ from fastcrud import FastCRUD
 
 from .models import FamilyConnection
 
-crud_family_connection: FastCRUD = FastCRUD(FamilyConnection)
+crud_family_connection: FastCRUD = FastCRUD(FamilyConnection, is_deleted_column="is_deleted", deleted_at_column="deleted_at")
